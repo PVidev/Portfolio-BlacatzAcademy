60936778638a7d56fd80198274e8db6ce1aad36a
Peyo Videv <107483422+PVidev@users.noreply.github.com>
2023-12-24 11:18:01 +0000

    PVidev

---
+++ React/practice1.1/src/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+
+const Menu = () => {
+  return (
+    <div>
+      <Button label="Practice-1" />
+      <Button label="Practice-2" />
+      <Button label="Practice-3" />
+      <Button label="Practice-4" />
+      <Button label="Practice-5" />
+      <Button label="Practice-6" />
+    </div>
+  );
+};
+
+export default Menu;
