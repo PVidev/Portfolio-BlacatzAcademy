cb6f7a22c2f0c6af09d70bf3e2bd086670f7ac30
Peyo Videv <107483422+PVidev@users.noreply.github.com>
2024-06-08 18:11:31 +0300

    Update 06-uniquePin.js

--- igra/06-uniquePin.js
+++ igra/06-uniquePin.js
@@ -4,23 +4,25 @@ function uniquePin(input) {
     let thirdUpperBound = Number(input[2]);
 
     for (let first = 2; first <= firstUpperBound; first += 2) {
-        for (let second = 2; second <= secondUpperBound; second++) {
-            let isSecondPrime = true;
-            if (second <= 1) {
-                isSecondPrime = false;
-            } else {
-                for (let i = 2; i <= Math.sqrt(second); i++) {
-                    if (second % i === 0) {
-                        isSecondPrime = false;
-                        break;
+        for (let third = 2; third <= thirdUpperBound; third += 2) {
+            for (let second = 2; second <= secondUpperBound; second++) {
+                let isSecondPrime = true;
+                if (second <= 1 || second > 7) {
+                    isSecondPrime = false;
+                } else {
+                    for (let i = 2; i <= Math.sqrt(second); i++) {
+                        if (second % i === 0) {
+                            isSecondPrime = false;
+                            break;
+                        }
                     }
                 }
-            }
-            for (let third = 2; third <= thirdUpperBound; third += 2) {
-                console.log(`${first} ${isSecondPrime ? second : 2} ${third}`);
+                if (isSecondPrime) {
+                    console.log(`${first} ${second} ${third}`);
+                }
             }
         }
     }
 }
 
-uniquePin(["8", "2", "8"]);
+uniquePin(["5", "3", "5"]);
